2c2c6074fe7d08ef9320144f05dfbf66793535d2
amitdatta <the.amistudy@gmail.com>
2017-07-30 18:38:13 +0530

    Initial commit

---
+++ src/main/java/com/amisoft/ch1/ch1_1_demo_volatile.java
@@ -0,0 +1,50 @@
+package com.amisoft.ch1;
+
+import java.util.concurrent.TimeUnit;
+
+public class ch1_1_demo_volatile {
+
+    public static void main(String []args){
+
+        DemoWorker worker = new DemoWorker();
+        Thread t1 = new Thread(worker);
+        t1.start();
+
+        try {
+            TimeUnit.MILLISECONDS.sleep(200);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        worker.setTerminated(true);
+        System.out.println("Finished....");
+
+    }
+}
+
+
+class DemoWorker implements  Runnable{
+
+   private volatile boolean isTerminated = false;
+
+    @Override
+    public void run() {
+
+        while(!isTerminated){
+            System.out.println("Hello from DemoWorker....");
+            try {
+                TimeUnit.MICROSECONDS.sleep(300);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public boolean isTerminated() {
+        return isTerminated;
+    }
+
+    public void setTerminated(boolean terminated) {
+        isTerminated = terminated;
+    }
+}
